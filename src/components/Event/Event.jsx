6999eea8bc46a8349331f0366a28c4fdb201a07a
gyorgynagy1986 <gyorgynagy1986@gmail.com>
2024-01-23 13:53:53 +0100

    text

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -6,14 +6,14 @@ import { Grand_Hotel } from "next/font/google";
 const font = Grand_Hotel({ subsets: ["latin"], weight: "400" });
 
 const textContent = {
-  h2: "Szeretnél egy rendezvényt a Negro Mangalicaban?",
+ // h2: "Szeretnél egy rendezvényt a Negro Mangalicaban?",
   p: "Hívj minket, vagy írj nekünk, és egyeztetjük a részleteket!",
   btn: "kapcsolat",
   url: "/kapcsolat",
 };
 
 const textContentEn = {
-  h2: "Insterested in hosting an event at Negro Mangalica?",
+  // h2: "Insterested in hosting an event at Negro Mangalica?",
   p: "Call us or send an e-mail if you’d like to organize an event in our restaurant!",
   btn: "Get in touch",
   url: "/en/contact",
@@ -28,7 +28,7 @@ const Event = ({ lang }) => {
   return (
     <section className={style.container}>
       <div className={style.containerText}>
-       {lang ? <h2 className={font.className}>Insterested in hosting an event at <span style={{whiteSpace:"nowrap", lineHeight:"145%"}}>Negro Mangalica?</span> </h2> : <h2 className={font.className}>Szeretnél egy rendezvényt a <span style={{whiteSpace:"nowrap", lineHeight:"145%"}}>Negro Mangalica-ban?</span></h2>}
+       {lang ? <h2 className={font.className}>Insterested in hosting an event at <span style={{whiteSpace:"nowrap", lineHeight:"145%"}}>Negro Mangalica?</span> </h2> : <h2 className={font.className}>Szeretnél egy rendezvényt a <span style={{whiteSpace:"nowrap", lineHeight:"145%"}}>Negro Mangalicaban?</span></h2>}
         <p className={style.h2}>{pLogic}</p>
         <Button
           btnBlue={true}
